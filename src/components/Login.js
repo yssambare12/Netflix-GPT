675f14ad30c9bfd27ce45c769401476de5e46333
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-13 14:44:53 +0530

    fix:logo not rendering

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,15 +9,18 @@ const Login = () => {
   };
 
   return (
-    <div className="">
-      <Header />
-      <div className="absolute">
+    <div className="relative min-h-screen">
+      <div className="relative z-20">
+        <Header />
+      </div>
+      <div className="absolute inset-0 z-0">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/7d2359a4-434f-4efa-9ff3-e9d38a8bde7f/web/IN-en-20250707-TRIFECTA-perspective_4faa9280-a2c5-4e07-aafc-a45ce43fea09_large.jpg"
           alt="banner"
+          className="w-full h-full object-cover"
         />
       </div>
-      <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 bg-opacity-80 text-white">
+      <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 bg-opacity-80 text-white z-10 rounded-md flex flex-col">
         <h1 className="font-bold text-3xl py-4">
           {isSigninform ? "Sign in" : "Sign Up"}
         </h1>
