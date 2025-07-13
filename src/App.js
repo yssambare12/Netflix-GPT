72371bbe6fb34ba38bea1bee139b2ba3133ef637
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-13 13:14:17 +0530

    chore:setup initial routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='bg-amber-200 text-3xl font-bold text-green-800'>This is test</h1>
-    </div>
+    <Body />
   );
 }
 
