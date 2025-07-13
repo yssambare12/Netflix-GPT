eb8efde34ed5d47ee3c8527605e5420b6e7d9176
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-13 14:07:29 +0530

    chore:created login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 const Header = () => {
-    return (
-
-    );
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black">
+      <img
+        className="w-44"
+        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-07-01/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"
+      />
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,33 @@
+import Header from "./Header";
+
 const Login = () => {
-    return (
-        <div className="">
-            <h1>Login</h1>
-        </div>
-    );
+  return (
+    <div className="">
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/7d2359a4-434f-4efa-9ff3-e9d38a8bde7f/web/IN-en-20250707-TRIFECTA-perspective_4faa9280-a2c5-4e07-aafc-a45ce43fea09_large.jpg"
+          alt="banner"
+        />
+      </div>
+      <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 bg-opacity-80 text-white">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email"
+          className="p-4 my-4 w-full bg-gray-900"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-900"
+        />
+        <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
