a074f7a2253fca5ed857f8926b2f16f0e47578d5
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-20 17:12:51 +0530

    chore:used useSelector to read redux data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
 import { signOut } from "firebase/auth";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
   const handleSignout = () => {
     signOut(auth)
       .then(() => {
@@ -22,21 +24,27 @@ const Header = () => {
         src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-07-01/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
-      <div className="flex items-center space-x-4">
-        <img
-          alt="userprofile"
-          className="rounded-full border border-gray-400"
-          width={45}
-          height={45}
-          src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
-        />
-        <button
-          onClick={handleSignout}
-          className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition"
-        >
-          Sign Out
-        </button>
-      </div>
+      {user && (
+        <div className="flex items-center space-x-4">
+          <img
+            alt="userprofile"
+            className="rounded-full border border-gray-400"
+            width={45}
+            height={45}
+            src={
+              user?.photoURL ||
+              "https://avatars.githubusercontent.com/u/91533131?v=4"
+            }
+          />
+          <span className="text-white font-semibold">{user?.displayName}</span>
+          <button
+            onClick={handleSignout}
+            className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition"
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
