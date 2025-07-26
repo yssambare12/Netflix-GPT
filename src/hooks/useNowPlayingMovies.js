f3ce8bae219cd58034265eb0886ea0bed0e821a6
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-26 16:42:45 +0530

    chore: created custom hook

---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+  const getNowPlayingMovide = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?age=1",
+      API_OPTIONS
+    );
+
+    const json = await data.json();
+
+    dispatch(addNowPlayingMovies(json.results));
+
+    console.log(json.results);
+  };
+
+  useEffect(() => {
+    getNowPlayingMovide();
+  }, []);
+};
+
+export default useNowPlayingMovies;
