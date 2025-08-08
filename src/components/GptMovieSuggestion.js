3d76900994a2d9597f79ccbeaea91881062a8b85
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-08 07:39:44 +0530

    Fix: TypeError in MovieList component and prevent potential errors

--- src/components/GptMovieSuggestion.js
+++ src/components/GptMovieSuggestion.js
@@ -4,18 +4,23 @@ import MovieList from "./MovieList";
 const GptMovieSuggestion = () => {
   const { movieName, movieSearchResult } = useSelector((store) => store.gpt);
 
-  console.log(movieName);
-  console.log(movieSearchResult);
+  
 
-  if (!movieName) {
-    //show shimmer ui
+  if (!movieName || !movieSearchResult) {
+    
     return null;
   }
 
   return (
     <div className="p-4 m-8 bg-black text-white z-50">
       <div>
-        <MovieList title={movieName[0]} movies={movieSearchResult[0]} />
+        {movieName.map((movieName, index) => (
+          <MovieList
+            key={movieName}
+            title={movieName}
+            movies={movieSearchResult[index]}
+          />
+        ))}
       </div>
     </div>
   );
