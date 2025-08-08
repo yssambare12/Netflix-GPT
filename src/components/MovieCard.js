0ac0a384f107fca66abf90159efbc01302ad85ea
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-08 08:13:06 +0530

    chore:updated moviecard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,7 +2,9 @@ import { ImageCDN } from "../utils/constants";
 
 const MovieCard = ({ posterPaths }) => {
   // const { title } = moviesData;
-
+  if (!posterPaths) {
+    return null;
+  }
   return (
     <div className="w-48 pr-4">
       <img src={ImageCDN + posterPaths} alt="postet" />
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,16 +1,15 @@
 import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
-  if(!movies) return
+  if (!movies || !Array.isArray(movies) || movies.length === 0) return null;
   return (
     <div className="px-6">
       <h1 className="text-3xl py-4 text-white">{title}</h1>
       <div className="flex overflow-x-scroll">
         <div className="flex">
-          {movies &&
-            movies.map((movie) => (
-              <MovieCard key={movie.id} posterPath={movie.poster_path} />
-            ))}
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} posterPaths={movie.poster_path} />
+          ))}
         </div>
       </div>
     </div>
