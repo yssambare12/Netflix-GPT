4dfaf9b199ae7a0de70b58a9f6cac15d509199cb
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-09 18:39:32 +0530

    chore: on movie click show the movie id

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,20 @@
 import { ImageCDN } from "../utils/constants";
 
-const MovieCard = ({ posterPaths }) => {
+const MovieCard = ({ posterPaths, movieId, onClick }) => {
   // const { title } = moviesData;
   if (!posterPaths) {
     return null;
   }
+
+  const handleClick = () => {
+    if (onClick && movieId) {
+      onClick(movieId);
+    }
+  };
+
   return (
-    <div className="w-48 pr-4">
-      <img src={ImageCDN + posterPaths} alt="postet" />
+    <div className="w-48 pr-4 cursor-pointer" onClick={handleClick}>
+      <img src={ImageCDN + posterPaths} alt="poster" />
       {/* <h3>{title}</h3> */}
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,13 +2,23 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
   if (!movies || !Array.isArray(movies) || movies.length === 0) return null;
+
+  const handleMovieClick = (movieId) => {
+    alert(`Movie ID: ${movieId}`);
+  };
+
   return (
     <div className="px-6">
       <h1 className="text-3xl py-4 text-white">{title}</h1>
       <div className="flex overflow-x-scroll">
         <div className="flex">
           {movies.map((movie) => (
-            <MovieCard key={movie.id} posterPaths={movie.poster_path} />
+            <MovieCard
+              key={movie.id}
+              posterPaths={movie.poster_path}
+              movieId={movie.id}
+              onClick={handleMovieClick}
+            />
           ))}
         </div>
       </div>
